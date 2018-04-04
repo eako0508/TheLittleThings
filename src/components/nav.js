45f3adcf6208442e805f460577ecb057fbdc85a2
Eugene <eako0508@gmail.com>
2018-04-04 08:51:32 -0400

    changed search tool small size

--- src/components/nav.js
+++ src/components/nav.js
@@ -27,10 +27,10 @@ export default class NavBS extends React.Component{
 				<Navbar.Collapse >
 					<Nav>
 						<NavItem>
-							<Input className='search-input'/>
+							<Input size='small' className='search-input'/>
 						</NavItem>
 						<NavItem>
-							<Button type='submit' className='search-btn'>Search</Button>							
+							<Button type='submit' size='small' className='search-btn'>Search</Button>							
 						</NavItem>
 					</Nav>
 					<Nav pullRight>
